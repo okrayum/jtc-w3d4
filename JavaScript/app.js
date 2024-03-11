eca471bde804617f4c27baaa2e39ece0f05170a2
Jason Morgan <morgannj71@gmail.com>
2024-03-11 11:55:17 -0700

    updated app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,80 @@
 const db = require("./db")
 
-db.run("INSERT INTO users (username, email) VALUES (?, ?)", ["Admin", "Admin@mail.com"], function (err) {
+// // Create a user here
+// db.run("INSERT INTO users (username, email) VALUES (?, ?)", ["Admin2", "Admin2@mail.com"], (err) => {
+//   if (err) {
+//     console.log("Error creating user:", err);
+//     return;
+//   }
+//   console.log("User created successfully");
+
+//   db.all("SELECT * FROM users", [], (err, rows) => {
+//     if (err) {
+//       console.error("Error retrieving data:", err);
+//       return;
+//     }
+//     console.log("All users:", rows);
+//   });
+// });
+
+// // Update user email here
+// db.run("UPDATE users SET email=? WHERE username=?", ["jason123@mail.com", "jason@jtc"], (err) => {
+//   if (err) {
+//     console.log("Error updating email:", err);
+//     return;
+//   }
+//   console.log("Email changed!");
+
+//   db.all("SELECT * FROM users", [], (err, rows) => {
+//     if (err) {
+//       console.error("Error retrieving data:", err);
+//       return;
+//     }
+//     console.log("All users:", rows);
+//   });
+// })
+
+// // Create a post here
+// db.run("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", ["4", "Site Maintenance", "Site will be down this Saturday."], function (err) {
+//   if (err) {
+//     console.log("Error creating post:", err);
+//     return;
+//   }
+//   console.log("Post created!");
+
+//   db.all("SELECT * FROM posts", [], (err, rows) => {
+//     if (err) {
+//       console.error("Error retrieving data:", err);
+//       return;
+//     }
+//     console.log("All posts:", rows);
+//   });
+// });
+
+// // Create a comment here
+// db.run("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", ["3", "1", "Is class earlier or later now?"], function (err) {
+//   if (err) {
+//     console.log("Error creating comment:", err);
+//     return;
+//   }
+//   console.log("Comment created!");
+
+//   db.all("SELECT * FROM comments", [], (err, rows) => {
+//     if (err) {
+//       console.error("Error retrieving data:", err);
+//       return;
+//     }
+//     console.log("All comments:", rows);
+//   });
+// });
+
+// Delete a user here
+db.run("DELETE FROM users WHERE username=?", ["Admin"], (err) => {
   if (err) {
-    console.log("Error creating user:", err);
+    console.log("Error deleting user:", err);
     return;
   }
-  console.log("User created successfully");
+  console.log("User deleted!");
 
   db.all("SELECT * FROM users", [], (err, rows) => {
     if (err) {
@@ -13,38 +82,6 @@ db.run("INSERT INTO users (username, email) VALUES (?, ?)", ["Admin", "Admin@mai
       return;
     }
     console.log("All users:", rows);
-  });
-});
-
-db.run("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", ["4", "Site Maintenance", "Site will be down this Saturday."], function (err) {
-  if (err) {
-    console.log("Error creating post:", err);
-    return;
-  }
-  console.log("Post created successfully");
-
-  db.all("SELECT * FROM posts", [], (err, rows) => {
-    if (err) {
-      console.error("Error retrieving data:", err);
-      return;
-    }
-    console.log("All posts:", rows);
-  });
-});
-
-db.run("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", ["3", "1", "Is class earlier or later now?"], function (err) {
-  if (err) {
-    console.log("Error creating comment:", err);
-    return;
-  }
-  console.log("Comment created successfully");
-
-  db.all("SELECT * FROM comments", [], (err, rows) => {
-    if (err) {
-      console.error("Error retrieving data:", err);
-      return;
-    }
-    console.log("All comments:", rows);
   });
 });
 
