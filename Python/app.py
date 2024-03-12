5380c28665f5b323f26c6e1e0607fec05a8c9195
Jason Morgan <morgannj71@gmail.com>
2024-03-11 20:27:20 -0700

    push completed assignment

--- Python/app.py
+++ Python/app.py
@@ -3,79 +3,81 @@ from schema import user_schema, post_schema, comment_schema
 
 con = sqlite3.connect("blog_app2.db")
 
+# con.execute("PRAGMA foreign_keys = ON;")
+
 cur = con.cursor()
 
 cur.execute(user_schema)
 cur.execute(post_schema)
 cur.execute(comment_schema)
 
-# Create a user here
-try:
-  username = "johnny"
-  email = "johnny@mail.com"
+# # Create a user here
+# try:
+#   username = "johnny"
+#   email = "johnny@mail.com"
 
-  cur.execute("INSERT INTO users (username, email) VALUES (?, ?)", ("sammy123", "sammy123@mail.com"))
-  print("User created!")
-except sqlite3.IntegrityError as e:
-  print("User not created successfully:", e)
+#   cur.execute("INSERT INTO users (username, email) VALUES (?, ?)", (username, email))
+#   print("User created!")
+# except sqlite3.IntegrityError as e:
+#   print("User not created successfully:", e)
 
-# Update a users email here
-try:
-  email = "test_mail@mail.com"
-  username = "unit_test_user5"
+# # Update a users email here
+# try:
+#   email = "test_mail@mail.com"
+#   username = "unit_test_user5"
 
-  cur.execute("UPDATE users SET email=? WHERE username=?", (email, username))
-  if cur.rowcount == 0:
-    print("No were changes made.")
-  else:
-    print("Email updated!")  
-except sqlite3.Error as e:
-  print("Error occurred.", e)  
+#   cur.execute("UPDATE users SET email=? WHERE username=?", (email, username))
+#   if cur.rowcount == 0:
+#     print("No changes were made.")
+#   else:
+#     print("Email updated!")  
+# except sqlite3.Error as e:
+#   print("Error occurred.", e)  
 
-# Update a users username here
-try:
-  new_username = "sammy"
-  old_username = "sammy123"
+# # Update a users username here
+# try:
+#   new_username = "sammy"
+#   old_username = "sammy123"
 
-  cur.execute("UPDATE users SET username=? WHERE username=?", (new_username, old_username))
-  if cur.rowcount == 0:
-    print("No were changes made.")
-  else:
-    print("Username updated!")
-except sqlite3.Error as e:
-  print("Error occurred:", e)    
+#   cur.execute("UPDATE users SET username=? WHERE username=?", (new_username, old_username))
+#   if cur.rowcount == 0:
+#     print("No changes were made.")
+#   else:
+#     print("Username updated!")
+# except sqlite3.Error as e:
+#   print("Error occurred:", e)    
 
-# Create a post here
-try:
-  user_id = "9"
-  title = "Another test"
-  content = "Testing 123"
+# # Create a post here
+# try:
+#   user_id = "11"
+#   title = "Another test22"
+#   content = "Testing 12322"
 
-  cur.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", (user_id, title, content))
-  if cur.rowcount == 0:
-    print("Post not created.")
-  else:
-    print("Post created!")  
-except sqlite3.Error as e:
-  print("Error occurred:", e)
+#   cur.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", (user_id, title, content))
+#   if cur.rowcount == 0:
+#     print("Post not created.")
+#   else:
+#     print("Post created!")  
+# except sqlite3.Error as e:
+#   print("Error occurred:", e)
 
-# Create a comment here
-try:
-  user_id = "9"
-  post_id = "1"
-  content = "Testing"
+# # Create a comment here
+# try:
+#   user_id = "9"
+#   post_id = "1"
+#   content = "Testing"
 
-  cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (user_id, post_id, content))
-  if cur.rowcount == 0:
-    print("Comment not created.")
-  else:
-    print("Comment created!")
-except sqlite3.Error as e:
-  print("Error occurred:". e)      
+#   cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (user_id, post_id, content))
+#   if cur.rowcount == 0:
+#     print("Comment not created.")
+#   else:
+#     print("Comment created!")
+# except sqlite3.Error as e:
+#   print("Error occurred:". e)      
 
 # Delete a user here
 try:
-  username = "rita"
+  username = "unit_tester"
 
   cur.execute("DELETE FROM users WHERE username=?", (username,))
   if cur.rowcount == 0:
